805f19e52e3f62f6be9fe957a7a6f63b913e6eb0
Vinay Kumar G Jain <vinay.g.jain@gmail.com>
2014-09-26 15:44:33 -0400

    Update build.sh
    Reverting to original

--- qemu/build.sh
+++ qemu/build.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
-
 python ../scripts/apigen.py
-
 ./configure --target-list=x86_64-softmmu,i386-softmmu,arm-softmmu \
 --cc=gcc-4.7 \
 --cxx=g++-4.7 \
 --prefix=`pwd`/install \
---cpu=x86_64 \
 --disable-pie \
+--enable-llvm \
+--with-llvm=../llvm-3.3/Debug+Asserts \
+--extra-cflags="-O2" \
+--extra-cxxflags="-O2" \
 && make -j $(nproc)
-# --disable-llvm \
-# --with-llvm=../../llvm-3.3/Release \
-# --extra-cflags="-O2" \
-# --extra-cxxflags="-O2" \
